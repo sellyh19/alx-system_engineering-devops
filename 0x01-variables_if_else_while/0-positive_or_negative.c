a41b660d5b4d9af3e6e75cfa67c913ca5217e32e
Selma Shikone <sellyh19@gmail.com>
2022-09-09 13:36:24 -0700

    Positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main- variable n is positive or negative
+ * Return: 0
+ */
+int main(void)
+{
+int n;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+return (0);
+}
